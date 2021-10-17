5ef02f4aee18aa73d4375906e3da198bad190990
Vitor Souza <79655309+vituvitaminadev@users.noreply.github.com>
2021-10-17 14:38:19 -0300

    Update IMC

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ function calcularIMC(){
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
+    if(1 > altura > 2.2 | 30 > peso > 300){
+        alert("O IMC é um medidor válido somente para adultos! Confira se digitou os dados corretamente!");
+    }
+
     if (nome !== '' && altura !== '' && peso !== '') {
         const valorIMC = (peso/(altura**2)).toFixed(1);
         
         let classificacao = '';
+        var peso_minimo = Math.round((altura ** 2) * 18.5);
+        var peso_maximo = Math.round((altura ** 2) * 25);
+        var faixa_peso = `Tente manter seu peso entre ${peso_minimo}kg e ${peso_maximo}kg.`; 
 
         if (valorIMC < 18.5){
             classificacao = 'abaixo do peso.';
@@ -23,7 +30,7 @@ function calcularIMC(){
             classificacao = 'com obesidade grau III.';
         }
 
-        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}`;
+        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}\n ${faixa_peso}`;
 
     }else {
         resultado.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
